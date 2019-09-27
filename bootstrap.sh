b18da73de4dce13de516dce7a8c34bfc5927e296
Lucas <lucas.vanstaden@enjo.com.au>
2019-09-27 13:58:25 +0800

    add in ngrok

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,13 +18,6 @@ wget -q https://raw.githubusercontent.com/git/git/v${GIT_VESRION}/contrib/comple
 mv ./git-completion.bash /home/vagrant
 source /home/vagrant/git-completion.bash
 
-echo fetching ngrok
-wget  -q --show-progress --progress=bar:force 2>&1 https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
-unzip ngrok-stable-linux-amd64.zip
-mv ./ngrok /usr/bin/
-rm -rf ngrok-stable-linux-amd64.zip
-
-
 envsubst '${DEV_DOMAIN}' < /etc/nginx/sites-available/magento > /etc/nginx/sites-enabled/magento
 service nginx stop
 service nginx start
