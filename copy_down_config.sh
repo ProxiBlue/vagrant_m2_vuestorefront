80eb30eb953097e82ab86f364e86d8cb81d08e6f
Lucas <lucas.vanstaden@enjo.com.au>
2020-05-28 08:46:10 +0800

    deleted old file

--- copy_down_config.sh
+++
@@ -1,4 +0,0 @@
-scp -i ~/.ssh/enjo_aws_private_key.pem ubuntu@magento.656ea.com:/var/www/html/current/app/etc/config.php ./sites/magento2/app/etc/config.php 
-scp -i ~/.ssh/enjo_aws_private_key.pem ubuntu@magento.656ea.com:/var/www/html/current/app/etc/env.php ./sites/magento2/app/etc/env.php.live 
-scp -i ~/.ssh/enjo_aws_private_key.pem ubuntu@www.656ea.com:/var/www/storefront/config/local.json ./sites/vue-storefront/config/local.json.live 
-scp -i ~/.ssh/enjo_aws_private_key.pem ubuntu@www.656ea.com:/var/www/api/config/local.json ./sites/vue-storefront-api/config/local.json.live 
